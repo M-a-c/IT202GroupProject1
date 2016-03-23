1a1f1e8537556cea77526324cb5840c3df12a88b
Mac Carter <M-a-c>
2016-03-23 05:13:04 -0500

    Figured out graphing
    Might need to refine graphing a bit.
    Need to Add more colors?
    Need to get heat from sensor.
    Need to get humidity from sensor.

--- RoomScript.js
+++ RoomScript.js
@@ -1,62 +1,312 @@
-var masterPoints = [];
-//http://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas
-//Posted by jaya
-// point.IsMaster=0;
-// point.signatureName='';
-// point.heatSignature=0;
-// point.Unit="Ferinheight";
-// point.Humidity=0;
-// point.x=0;
-// point.y=0;
 
-Math.dist=function(x1,y1,x2,y2){ 
+
+
+var masterPoints = [];
+var humidMap;
+var heatMap;
+var horizontal;
+var Vertical;
+var dx;
+var dy;
+var alpha = .5;
+
+var temp=30;
+
+
+/********************************/
+/*								*/
+/*	Read temperature data and 	*/
+/*	other data in funcitons		*/
+/*			below.				*/
+/*								*/
+/********************************/
+
+function getTemperature(no){
+	//Make some request.
+	temp = temp+9;
+	return temp+9;
+}
+function getHumidity(no){
+	//Make some request.
+	temp = temp+9;
+	return temp+9;
+}
+
+
+//http://stackoverflow.com/questions/20916953/get-distance-between-two-points-in-canvas
+function distance(x1,y1,x2,y2){ 
   if(!x2) x2=0; 
   if(!y2) y2=0;
   return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)); 
 }
-function test(){
-	alert("test");
+
+
+
+
+
+function addSensor(x,y,no,name){
+	//Get the context of both windows.
+	var ctx1 = humidMap.getContext("2d");
+	var ctx2 = heatMap.getContext("2d");
+
+	//Fill the points as black dots for now...
+	ctx2.fillRect(x*dx,y*dy,dx,dy);
+	ctx2.stroke();
+	ctx1.fillRect(x*dx,y*dy,dx,dy);
+	ctx1.stroke();
+
+	var tempHumidity = getHumidity(no);
+	var tempTemperature = getTemperature(no);
+
+	masterPoints.push({x,y,no,name, tempHumidity, tempTemperature, distance: 0, fraciton: 0 });
 }
 
-// function addHeatSignature (x,y,signatureName,) {
-	
-// 	point.IsMaster=0;
-// 	point.signatureName='';
-// 	point.heatSignature=0;
-// 	point.Unit="Ferinheight";
-// 	point.Humidity=0;
-// 	point.x=0;
-// 	point.y=0;
+function interpolateHumidity(ctx,x,y){
+	var tempTotalDistance=0;
 
+	for (var i = masterPoints.length - 1; i >= 0; i--) {
+		if (masterPoints[i].y == y/dy && masterPoints[i].x == x/dx) {
+			ctx.fillStyle = humidColorArray[masterPoints[i].tempTemperature]+","+alpha+")";
+			ctx.fillRect(x,y,dx,dy);
+			return;
+		}
+
+		var temp = Math.floor(distance(x/dx,y/dy,masterPoints[i].x,masterPoints[i].y));
+		masterPoints[i].distance = (1/temp);
+		tempTotalDistance += (1/temp);
+	}
+	
+	var ret=0;
+	// var killme=0;
+	for (var i = masterPoints.length - 1; i >= 0; i--) {
+		masterPoints[i].fraciton = (masterPoints[i].distance/tempTotalDistance);
+		ret += (masterPoints[i].fraciton*masterPoints[i].tempHumidity);
+	}
+	// console.log("?");
+	if((Math.floor(ret)/10)<10){
+		ctx.fillStyle = humidColorArray[(Math.floor(ret))]+","+alpha+")";
+		ctx.fillRect(x,y,dx,dy);
+	}
+
+	// return ret;
+}
+
+
+// function interpolateHumidity(ctx,x,y){
+// 	var tempTotalDistance=0;
+
+// 	for (var i = masterPoints.length - 1; i >= 0; i--) {
+// 		if (masterPoints[i].y == y/dy && masterPoints[i].x == x/dx) {
+// 			return;
+// 		}
+
+// 		var temp = Math.floor(distance(x/dx,y/dy,masterPoints[i].x,masterPoints[i].y));
+// 		masterPoints[i].distance = (1/temp);
+// 		tempTotalDistance += (1/temp);
+// 	}
+	
+// 	var ret=0;
+// 	// var killme=0;
+// 	for (var i = masterPoints.length - 1; i >= 0; i--) {
+// 		masterPoints[i].fraciton = (masterPoints[i].distance/tempTotalDistance);
+// 		ret += (masterPoints[i].fraciton*masterPoints[i].tempTemperature);
+// 	}
+
+// 	return ret;
 // }
 
-function drawBoxes(canvas,horizontalSquares,VerticalSquares){
 
-// var c=document.getElementById("myCanvas");
-// var ctx=c.getContext("2d");
-	var ctx = canvas.getContext("2d");
-	X = horizontalSquares;
-	Y = VerticalSquares;
+function p(){
+	for (var i = masterPoints.length - 1; i >= 0; i--) {
+		console.log(masterPoints[i]);
+	
+	}
+	var tmp=0;
+	for (var i = masterPoints.length - 1; i >= 0; i--) {
+		tmp+=masterPoints[i].fraciton;
+	}
+	console.log(tmp);
+}
+
+function updateAll(){
+	for (var i = masterPoints.length - 1; i >= 0; i--) {
+		// console.log(masterPoints[i]);
+		//Read new data.
+	}
+
+	updateMap();
+}
+
+function drawBoxes(horizontalSquares,VerticalSquares){
+	horizontal = horizontalSquares;
+	Vertical = VerticalSquares;
+
+	updateMap();
+}
+
+
+function updateMap(){
+	//Updates the rest of the map.
+	var ctx1 = heatMap.getContext("2d");
+	var ctx2 = humidMap.getContext("2d");
+	X = horizontal;
+	Y = Vertical;
+	
 	var xOffset = 0;
 	var yOffset = 0;
-	console.log("horizontalSquares: "+horizontalSquares);
-	console.log("VerticalSquares: "+VerticalSquares);
 
+	dx = ((humidMap.width) / (horizontal));
+	dy = ((humidMap.height) / (Vertical));
 
-	var dx = ((canvas.width) / (horizontalSquares));
-	var dy = ((canvas.height) / (VerticalSquares));
-
-	console.log("canvas.width: "+canvas.width);
 
 	//Setup the default state.
 	for (var j = 0; j<Y; j++) {
 		xOffset = 0;
 		for (var i = 0; i<X; i++) {
-			console.log("xoff: "+xOffset+" yoff:"+yOffset+" dx:"+dx+" dy:"+dy);
-			ctx.rect(xOffset,yOffset,dx,dy);
+			// console.log("xoff: "+xOffset+" yoff:"+yOffset+" dx:"+dx+" dy:"+dy);
+			
+			// 
+			// ctx.fillRect(20,20,150,100);
+			// ctx.stroke();
+			// 
+
+			// interpolateTemperature(ctx1,xOffset,yOffset);
+			// ctx1.rect(xOffset,yOffset,dx,dy);
+			interpolateHumidity(ctx2,xOffset,yOffset);
+			// ctx2.rect(xOffset,yOffset,dx,dy);
+
 			xOffset += dx;
 		}
+		// console.log(yOffset);
 		yOffset += dy;
 	}
-	ctx.stroke();
+	ctx1.stroke();
+	ctx2.stroke();
 }
+
+
+
+
+
+humidColorArray = [];
+//1 for each 10%
+//Whatever, no calculation needs to be done. its fine...
+humidColorArray[0] = "rgba(50, 0, 0";//DarkBrown
+humidColorArray[1] = "rgba(54, 7, 0";
+humidColorArray[2] = "rgba(58, 12, 0";
+humidColorArray[3] = "rgba(62, 19, 0";
+humidColorArray[4] = "rgba(66, 24, 0";
+humidColorArray[5] = "rgba(72, 30, 0";
+humidColorArray[6] = "rgba(76, 36, 0";
+humidColorArray[7] = "rgba(80, 42, 0";
+humidColorArray[8] = "rgba(85, 47, 0";
+humidColorArray[9] = "rgba(90, 51, 0";
+
+humidColorArray[10] = "rgba(95, 56, 0";//Brown
+humidColorArray[11] = "rgba(100, 60, 0";
+humidColorArray[12] = "rgba(105, 65, 0";
+humidColorArray[13] = "rgba(110, 70, 0";
+humidColorArray[14] = "rgba(115, 75, 0";
+humidColorArray[15] = "rgba(120, 81, 0";
+humidColorArray[16] = "rgba(123, 87, 0";
+humidColorArray[17] = "rgba(127, 94, 0";
+humidColorArray[18] = "rgba(130, 102, 0";
+humidColorArray[19] = "rgba(135, 108, 0";
+
+humidColorArray[20] = "rgba(140, 112, 0";//Light brown
+humidColorArray[21] = "rgba(144, 117, 0";
+humidColorArray[22] = "rgba(148, 123, 0";
+humidColorArray[23] = "rgba(152, 128, 0";
+humidColorArray[24] = "rgba(156, 134, 0";
+humidColorArray[25] = "rgba(160, 140, 0";
+humidColorArray[26] = "rgba(164, 146, 0";
+humidColorArray[27] = "rgba(168, 152, 0";
+humidColorArray[28] = "rgba(175, 158, 0";
+humidColorArray[29] = "rgba(180, 164, 0";
+
+humidColorArray[30] = "rgba(186, 170, 0";//Brass
+humidColorArray[31] = "rgba(189, 174, 0";
+humidColorArray[32] = "rgba(192, 178, 0";
+humidColorArray[33] = "rgba(195, 182, 0";
+humidColorArray[34] = "rgba(198, 186, 0";
+humidColorArray[35] = "rgba(201, 190, 0";
+humidColorArray[36] = "rgba(104, 194, 0";
+humidColorArray[37] = "rgba(108, 198, 0";
+humidColorArray[38] = "rgba(112, 202, 0";
+humidColorArray[39] = "rgba(116, 207, 0";
+
+humidColorArray[40] = "rgba(220, 212, 0";//Gold
+humidColorArray[41] = "rgba(223, 216, 0";
+humidColorArray[42] = "rgba(226, 220, 0";
+humidColorArray[43] = "rgba(229, 224, 0";
+humidColorArray[44] = "rgba(232, 228, 0";
+humidColorArray[45] = "rgba(235, 232, 0";
+humidColorArray[46] = "rgba(239, 236, 0";
+humidColorArray[47] = "rgba(243, 240, 0";
+humidColorArray[48] = "rgba(247, 245, 0";
+humidColorArray[49] = "rgba(251, 250, 0";
+
+
+humidColorArray[50] = "rgba(255, 255, 0";//Yellow
+humidColorArray[51] = "rgba(243, 255, 0";
+humidColorArray[52] = "rgba(231, 255, 0";
+humidColorArray[53] = "rgba(218, 255, 0";
+humidColorArray[54] = "rgba(205, 255, 0";
+humidColorArray[55] = "rgba(192, 255, 0";
+humidColorArray[56] = "rgba(179, 255, 0";
+humidColorArray[57] = "rgba(166, 255, 0";
+humidColorArray[58] = "rgba(153, 255, 0";
+humidColorArray[59] = "rgba(140, 255, 0";
+
+humidColorArray[60] = "rgba(127, 255, 0";//Green
+humidColorArray[61] = "rgba(121, 252, 0";
+humidColorArray[62] = "rgba(115, 248, 0";
+humidColorArray[63] = "rgba(109, 244, 0";
+humidColorArray[64] = "rgba(103, 240, 0";
+humidColorArray[65] = "rgba(97, 236, 0";
+humidColorArray[66] = "rgba(91, 232, 0";
+humidColorArray[67] = "rgba(85, 228, 0";
+humidColorArray[68] = "rgba(79, 224, 0";
+humidColorArray[69] = "rgba(73, 220, 0";
+
+humidColorArray[70] = "rgba(63, 216, 0";//Grass Green
+humidColorArray[71] = "rgba(57, 213, 0";
+humidColorArray[72] = "rgba(51, 210, 0";
+humidColorArray[73] = "rgba(45, 206, 0";
+humidColorArray[74] = "rgba(39, 202, 0";
+humidColorArray[75] = "rgba(33, 198, 0";
+humidColorArray[76] = "rgba(27, 194, 0";
+humidColorArray[77] = "rgba(21, 190, 0";
+humidColorArray[78] = "rgba(14, 186, 0";
+humidColorArray[79] = "rgba(7, 182, 0";
+
+humidColorArray[80] = "rgba(0, 178, 0";//Thick Green
+humidColorArray[81] = "rgba(0, 171, 0";
+humidColorArray[82] = "rgba(0, 164, 0";
+humidColorArray[83] = "rgba(0, 157, 0";
+humidColorArray[84] = "rgba(0, 150, 0";
+humidColorArray[85] = "rgba(0, 142, 0";
+humidColorArray[86] = "rgba(0, 134, 0";
+humidColorArray[87] = "rgba(0, 126, 0";
+humidColorArray[88] = "rgba(0, 118, 0";
+humidColorArray[89] = "rgba(0, 110, 0";
+
+humidColorArray[90] = "rgba(0, 102, 0";//Dark Green
+humidColorArray[91] = "rgba(0, 92, 0";
+humidColorArray[92] = "rgba(0, 82, 0";
+humidColorArray[93] = "rgba(0, 72, 0";
+humidColorArray[94] = "rgba(0, 62, 0";
+humidColorArray[95] = "rgba(0, 52, 0";
+humidColorArray[96] = "rgba(0, 42, 0";
+humidColorArray[97] = "rgba(0, 32, 0";
+humidColorArray[98] = "rgba(0, 22, 0";
+humidColorArray[99] = "rgba(0, 12, 0";
+
+humidColorArray[100] = "rgba(0, 12, 0";//Dark Green Same :D
+
+
+
+
+
+
+
